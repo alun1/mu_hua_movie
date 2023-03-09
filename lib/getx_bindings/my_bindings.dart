cd08dbc4aab566bceac4e5f9a54201393e522160
alun1 <903996130@qq.com>
2023-03-09 18:18:52 +0800

    很好

--- lib/getx_bindings/my_bindings.dart
+++ lib/getx_bindings/my_bindings.dart
@@ -1,10 +1,15 @@
 import 'package:get/get.dart';
 
 import '../model/repository/net_repository.dart';
+import '../service/my_service.dart';
 
 class MyBinding implements Bindings {
   @override
-  void dependencies() {
+  Future<void> dependencies() async {
     Get.put(NetRepository(), permanent: true);
+
+    final myService = MyService();
+    Get.put(myService, permanent: true);
+
   }
 }
