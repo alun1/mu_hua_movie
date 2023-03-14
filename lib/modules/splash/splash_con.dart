bc5b2966849174840541ee916b84c16dd204ecfb
alun1 <903996130@qq.com>
2023-03-14 11:24:51 +0800

    很好

--- lib/modules/splash/splash_con.dart
+++ lib/modules/splash/splash_con.dart
@@ -1,12 +1,13 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:mu_hua_movie/model/entity/update_info_entity.dart';
 import 'package:mu_hua_movie/model/repository/net_versoin_update_repository.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:mu_hua_movie/native_channel/native_channel.dart';
+import 'package:permission_handler/permission_handler.dart';
 import '../../routes/app_routes.dart';
 import '../../service/my_service.dart';
 import '../source/source_entity.dart';
@@ -29,7 +30,7 @@ class SplashCon extends GetxController {
       Hive.registerAdapter(SourceEntityAdapter());
     }
     PackageInfo packageInfo = await PackageInfo.fromPlatform();
-    buildNumber= packageInfo.buildNumber;
+    buildNumber = packageInfo.buildNumber;
     await Get.find<MyService>().initBox();
     await updateVersion();
     Get.offAndToNamed(Routes.home);
@@ -46,29 +47,62 @@ class SplashCon extends GetxController {
         var updateInfo = UpdateInfoEntity.fromJson(platformUpdateInfo);
 
         if (int.parse(buildNumber) < updateInfo.versionCode) {
-          Get.defaultDialog(
-              title: "更新",
-              content: Text(updateInfo.log),
-              confirm: ElevatedButton(
-                  onPressed: () async {
-                    Get.back();
-
-                    String path = await Get.find<NetVersionUpdateRepository>()
-                        .downApp(updateInfo.url, onDownloadProgress:
-                            (int byteCount, int totalBytes) {
-                      progress.value = byteCount / totalBytes;
-                    });
-
-                    await NativeChannel().installApk(path);
-                  },
-                  child: const Text("确定")),
-              cancel: ElevatedButton(
-                  onPressed: () {
-                    Get.back();
-                  },
-                  child: const Text("取消")));
+          String operatingSystem = Platform.operatingSystem;
+          if (operatingSystem == "android") {
+            await updateAndroid(updateInfo);
+          }
         }
       }
     }
+  }
+
+  updateAndroid(UpdateInfoEntity updateInfo) async {
+    bool isUpdate = await Get.defaultDialog(
+        title: "更新",
+        content: Text(updateInfo.log),
+        confirm: ElevatedButton(
+            onPressed: () async {
+              Get.back(result: true);
+            },
+            child: const Text("确定")),
+        cancel: TextButton(
+            onPressed: int.parse(buildNumber) < updateInfo.force
+                ? null
+                : () {
+                    Get.back(result: false);
+                  },
+            child: const Text("取消")));
+
+    if (isUpdate) {
+      bool isGranted = await androidInstallPermission();
+      if (isGranted) {
+        String path = await Get.find<NetVersionUpdateRepository>()
+            .downApp(updateInfo.url,
+                onDownloadProgress: (int byteCount, int totalBytes) {
+          progress.value = byteCount / totalBytes;
+        });
+
+        await NativeChannel().installApk(path);
+      } else {
+        if (int.parse(buildNumber) < updateInfo.force) {
+          Get.snackbar("提示", "用户授权失败！将推出app");
+          await Future.delayed(const Duration(seconds: 3));
+          SystemNavigator.pop();
+          exit(0);
+        } else {
+          Get.snackbar("提示", "用户授权失败！无法更新");
+        }
+      }
+    }
+  }
+
+  Future<bool> androidInstallPermission() async {
+    var status = await Permission.requestInstallPackages.request();
+    if (status.isGranted) {
+      return true;
+    } else if (status.isPermanentlyDenied) {
+      return await openAppSettings();
+    }
+    return false;
   }
 }
--- lib/modules/splash/splash_screen.dart
+++ lib/modules/splash/splash_screen.dart
@@ -9,21 +9,24 @@ class SplashScreen extends GetView<SplashCon> {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.teal,
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Icon(
-              Icons.play_arrow,
-              color: Colors.white,
-              size: 100,
-            ),
-            Obx(
-              () => Visibility(
-                  visible: controller.progress.value > 0,
-                  child: LinearProgressIndicator(
-                      value: controller.progress.value)),
-            )
-          ],
+        body: SizedBox(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Icon(
+                Icons.play_arrow,
+                color: Colors.white,
+                size: 100,
+              ),
+              Obx(
+                () => Visibility(
+                    visible: controller.progress.value > 0,
+                    child: LinearProgressIndicator(
+                        value: controller.progress.value)),
+              )
+            ],
+          ),
         ));
   }
 }
